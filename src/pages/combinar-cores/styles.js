5652d74c304a66a91fc1eb65909db1ba1c99645c
Victor Coelho <victor-matos8@hotmail.com>
2021-11-14 08:05:52 -0300

    Status do jogo e chegagem vencedor

--- src/pages/combinar-cores/styles.js
+++ src/pages/combinar-cores/styles.js
@@ -23,22 +23,76 @@ export const Body = styled.div`
 `;
 
 export const Stack = styled.div`
-    width: 70px;
-    height: 450px;
+    width: 50px;
+    height: 300px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-left: 10px; margin-right: 10px;
-    padding: 10px;
+    margin-left: 5px; margin-right: 5px;
+    padding: 15px;
     border-radius: 5px;
-    background-color: var(--color-dark);
+    background-color: var(--color-light-dark);
+    border: 3px solid var(--color-dark);
+    ${(props)=>{
+        switch (props.full) {
+            case 1:
+                return `
+                    border-color: var(--color-light-blue);
+                `;
+            case 2:
+                return `
+                    border-color: var(--color-light-pink);
+                `;
+            case 3:
+                return `
+                    border-color: var(--color-light-green);
+                `;
+            case 4:
+                return `
+                    border-color: var(--color-light-orange);
+                `;
+            case 5:
+                return `
+                    border-color: var(--color-dark-blue);
+                `;
+            default:
+                break;
+        }
+    }}
 `;
 
 export const Block = styled.div`
     user-select: none;
-    padding: 16px;
-    margin: 0 0 8px 0;
-    min-height: 50px;
-    background-color: ${(props)=>props.snapshot.isDragging? "#263B4A" : "#456C86"};
+    width: 100%;
+    height: 50px;
+    margin-bottom: 12px;
     color: white;
+    border-radius: 50%;
+
+    ${(props)=>{
+        switch (props.type) {
+            case 1:
+                return `
+                    background-color: var(--color-light-blue);
+                `;
+            case 2:
+                return `
+                    background-color: var(--color-light-pink);
+                `;
+            case 3:
+                return `
+                    background-color: var(--color-light-green);
+                `;
+            case 4:
+                return `
+                    background-color: var(--color-light-orange);
+                `;
+            case 5:
+                return `
+                    background-color: var(--color-dark-blue);
+                `;
+            default:
+                break;
+        }
+    }}
 `;
--- src/pages/combinar-cores/view.js
+++ src/pages/combinar-cores/view.js
@@ -14,63 +14,139 @@ import {
 
 const View = () => {
 
-    const [columns, setColumns] = useState({
-        [uuid()]: {
-          items: [
-            { id: uuid() },
-            { id: uuid() },
-            { id: uuid() }
-          ]
-        },
-        [uuid()]: {
-          items: []
-        },
-        [uuid()]: {
-          items: []
-        },
-        [uuid()]: {
-          items: []
-        },
-        [uuid()]: {
-          items: []
+    const [status, setStatus] = useState(0);
+
+    const Size = {
+        columns: 5,
+        rows: 4,
+        max: 5
+    };
+
+    const startGame = () => {
+        if(status === 1) return;
+        setStatus(1);
+    }
+
+    const restart = () => {
+        reset();
+        startGame();
+    }
+
+    const reset = () => {
+        setStatus(0);
+        if(status !== 0) setColumns(setupColumns());
+    }
+
+    const setupColumns = () => {
+        let rows = Size.rows;
+        let columnsSize = Size.columns;
+        let random = randomItems(columnsSize, rows);
+
+        let target = {};
+
+        for(let i = 0; i < columnsSize; i++) {
+
+            let items = [];
+
+            for(let iB = 0; iB < rows; iB++) {
+                items.push({ id: uuid(), type: random[(i * rows) + iB]});
+            }
+
+            target[uuid()] = {
+                items: items,
+                disabled: false,
+                full: false
+            };
         }
-    });
+
+        return target;
+    }
+
+    const randomItems = (columns, max) => {
+        let target = [];
+
+        for(let x = 0; x < columns; x++) {
+            for(let y = 0; y < max; y++) {
+                target[(x * max) + y] = x + 1;
+            }
+        }
+        target.sort(() => Math.random() - 0.5);
+        return target;
+    }
+
+    const checkWinner = (nColumns) => {
+        let allFull = true;
+        for(let item in nColumns) {
+            if(!nColumns[item].full) allFull = false;
+        }
+        if(allFull) setStatus(2);
+    }
+
+    const checkFull = (items) => {
+        if(items.length === Size.rows) {
+            let count = 0;
+            let last = 0;
+            for(let item in items) {
+                if(last === 0 || items[item].type === last) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                last = items[item].type;
+            }
+            if(count === Size.rows) return last;
+        }
+        return 0;
+    }
+
+    const [columns, setColumns] = useState(setupColumns);
     
     const onDragEnd = (result, columns, setColumns) => {
       if (!result.destination) return;
       const { source, destination } = result;
+      let nColumns = {};
     
       if (source.droppableId !== destination.droppableId) {
         const sourceColumn = columns[source.droppableId];
         const destColumn = columns[destination.droppableId];
         const sourceItems = [...sourceColumn.items];
+        const sourceDisabled = (sourceItems.length - 1) === Size.max;
         const destItems = [...destColumn.items];
+        const destDisabled = (destItems.length + 1) === Size.max;
         const [removed] = sourceItems.splice(source.index, 1);
         destItems.splice(destination.index, 0, removed);
-        setColumns({
+        const sourceFull = checkFull(sourceItems);
+        const destFull = checkFull(destItems);
+        nColumns = {
           ...columns,
           [source.droppableId]: {
             ...sourceColumn,
-            items: sourceItems
+            items: sourceItems,
+            disabled: sourceDisabled,
+            full: sourceFull
           },
           [destination.droppableId]: {
             ...destColumn,
-            items: destItems
+            items: destItems,
+            disabled: destDisabled,
+            full: destFull
           }
-        });
+        };
       } else {
         const column = columns[source.droppableId];
         const copiedItems = [...column.items];
         const [removed] = copiedItems.splice(source.index, 1);
         copiedItems.splice(destination.index, 0, removed);
-        setColumns({
+        nColumns = {
           ...columns,
           [source.droppableId]: {
             ...column,
             items: copiedItems
           }
-        });
+        };
       }
+      setColumns(nColumns);
+      checkWinner(nColumns);
     };
 
     useEffect(() => {
@@ -80,16 +156,19 @@ const View = () => {
         <>
             <Area>
                 <GameOptions 
-                    title="Combinar Cores"/>
+                    title="Combinar Cores"
+                    play={()=>startGame()}
+                    restart={()=>restart()}
+                    stop={()=>reset()}/>
                 <Container>
                     <Body>
                         <DragDropContext
                             onDragEnd={result => onDragEnd(result, columns, setColumns)}>
                             {Object.entries(columns).map(([columnId, column], index) => {
                                 return (
-                                    <Stack key={columnId}>
+                                    <Stack key={columnId} full={column.full}>
                                         <div style={{ width: "100%", height: 50 }}>
-                                            <Droppable droppableId={columnId} key={columnId}>
+                                            <Droppable droppableId={columnId} key={columnId} isDropDisabled={column.disabled || status !== 1}>
                                             {(provided, snapshot) => {
                                                 return (
                                                 <div
@@ -97,29 +176,31 @@ const View = () => {
                                                     ref={provided.innerRef}
                                                     style={{ width: "100%", minHeight: "100%" }}>
                                                     {column.items.map((item, index) => {
-                                                    return (
-                                                        <Draggable
-                                                        key={item.id}
-                                                        draggableId={item.id}
-                                                        index={index}
-                                                        >
-                                                        {(provided, snapshot) => {
-                                                            return (
-                                                            <Block
-                                                                snapshot={snapshot}
-                                                                ref={provided.innerRef}
-                                                                {...provided.draggableProps}
-                                                                {...provided.dragHandleProps}
-                                                                style={{
-                                                                    ...provided.draggableProps.style,
-                                                                    left: "0 !important",
-                                                                    top: "0 !important"
-                                                                }}>
-                                                            </Block>
-                                                            );
-                                                        }}
-                                                        </Draggable>
-                                                    );
+                                                        return (
+                                                            <Draggable
+                                                            key={item.id}
+                                                            draggableId={item.id}
+                                                            index={index}
+                                                            isDragDisabled={index !== 0 || status !== 1}
+                                                            >
+                                                            {(provided, snapshot) => {
+                                                                return (
+                                                                <Block
+                                                                    type={item.type}
+                                                                    snapshot={snapshot}
+                                                                    ref={provided.innerRef}
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}
+                                                                    style={{
+                                                                        ...provided.draggableProps.style,
+                                                                        left: "0 !important",
+                                                                        top: "0 !important"
+                                                                    }}>
+                                                                </Block>
+                                                                );
+                                                            }}
+                                                            </Draggable>
+                                                        );
                                                     })}
                                                     {provided.placeholder}
                                                 </div>
